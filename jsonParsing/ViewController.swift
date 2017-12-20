eabeab324b5e957b83b022f850c97649ff055880
abhishek bhatt <abhishekbhatt404@gmail.com>
2017-12-20 13:48:59 +0530

    commit

--- jsonParsing/ViewController.swift
+++ jsonParsing/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
+struct country: Decodable {
+    let name : String
+    let capital : String
+    let region : String
+}
+
 class ViewController: UIViewController {
+    var countries = [country]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let jsonURL = "https://restcountries.eu/rest/v2/all"
+        let url = URL(string: jsonURL)
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            do {
+                self.countries = try JSONDecoder().decode([country].self, from: data!)
+                for eachCountry in self.countries{
+                    print(eachCountry.name +      eachCountry.capital)
+                }
+            }
+        catch {
+            print("error")
+            }
+        
+        
+        
     }
-
+        .resume()
 
 }
 
+}
